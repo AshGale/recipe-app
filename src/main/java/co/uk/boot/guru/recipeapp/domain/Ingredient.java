0cd019fe3a193c64b555de24d1400dea83aa1960
AshGale <ashgale17@yahoo.co.uk>
2018-11-01 13:24:38 +0000

    added Uom, as a UniDirectional to Ingredient

--- src/main/java/co/uk/boot/guru/recipeapp/domain/Ingredient.java
+++ src/main/java/co/uk/boot/guru/recipeapp/domain/Ingredient.java
@@ -1,23 +1,38 @@
 package co.uk.boot.guru.recipeapp.domain;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 public class Ingredient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private  String description;
     private BigDecimal amount;
 
-    //private  UnitOfMeasure uom
+    @OneToOne(fetch = FetchType.EAGER)
+    private  UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
 
     public String getDescription() {
         return description;
