6cb2f06cb3a48cd17a0f9cd66f49ed256fe76383
AshGale <ashgale17@yahoo.co.uk>
2018-11-07 13:00:46 +0000

    enhanced junit index test

--- src/test/java/co/uk/boot/guru/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/co/uk/boot/guru/recipeapp/controllers/IndexControllerTest.java
@@ -1,17 +1,20 @@
 package co.uk.boot.guru.recipeapp.controllers;
 
 import co.uk.boot.guru.recipeapp.Services.RecipeService;
+import co.uk.boot.guru.recipeapp.domain.Recipe;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
 
@@ -32,9 +35,31 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+
+        //Given
+        Set<Recipe> recipeSet = new HashSet<>();
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipeSet.add(recipe);
+
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        recipeSet.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //When
         String viewName = controller.getIndexPage(model);
+
+        //then
         assertEquals("index",viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model, times(1))
+                .addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
     }
 }
