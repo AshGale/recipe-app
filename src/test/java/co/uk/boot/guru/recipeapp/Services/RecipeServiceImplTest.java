d9ad0f52492f5f276d9bce72027a2c887e35105e
AshGale <ashgale17@yahoo.co.uk>
2018-11-07 12:46:10 +0000

    Junit, added Catagory basic test, and index page test

---
+++ src/test/java/co/uk/boot/guru/recipeapp/Services/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package co.uk.boot.guru.recipeapp.Services;
+
+import co.uk.boot.guru.recipeapp.domain.Recipe;
+import co.uk.boot.guru.recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setup() throws Exception{
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeHashSet = new HashSet<>();
+        recipeHashSet.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeHashSet);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(),1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
