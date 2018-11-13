53183d2fe63f8356543e8798873eec569cdd7e18
AshGale <ashgale17@yahoo.co.uk>
2018-11-13 10:34:40 +0000

    added ground work for showing ingredients

---
+++ src/main/java/co/uk/boot/guru/recipeapp/controllers/IngredientController.java
@@ -0,0 +1,35 @@
+package co.uk.boot.guru.recipeapp.controllers;
+
+import co.uk.boot.guru.recipeapp.Services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+
+    RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{id}/ingredients")
+    public String listIngredients(@PathVariable String id, Model model){
+        log.debug("getting ingredients from recipe: " + id);
+//        Set<Ingredient> ingredientSet = new HashSet<>();
+//        ingredientSet = recipeService.findById(Long.valueOf(id)).getIngredients();
+//
+//        model.addAttribute("ingredients",ingredientSet);
+
+        model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/ingredient/list";
+    }
+
+}
